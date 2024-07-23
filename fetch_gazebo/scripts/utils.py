aeb4406e7756c3443361df50e26e046d8978efc0
07hokage <saihaneesh.allu@gmail.com>
2024-07-23 17:22:44 -0500

    utils

---
+++ fetch_gazebo/scripts/utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def compute_xyz(depth_img, fx, fy, px, py, height, width):
+    indices = np.indices((height, width), dtype=np.float32).transpose(1, 2, 0)
+    z_e = depth_img
+    x_e = (indices[..., 1] - px) * z_e / fx
+    y_e = (indices[..., 0] - py) * z_e / fy
+    xyz_img = np.stack([x_e, y_e, z_e], axis=-1)  # Shape: [H x W x 3]
+    return xyz_img
